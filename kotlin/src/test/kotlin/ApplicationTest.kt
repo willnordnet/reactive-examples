271e9e67d094f0298dcafada09f427b8251b4405
Will <will.gao@nordnet.se>
2024-12-04 11:33:34 +0100

    Add kotlin application

--- kotlin/src/test/kotlin/ApplicationTest.kt
+++ kotlin/src/test/kotlin/ApplicationTest.kt
@@ -2,7 +2,6 @@ import app.Application
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.web.reactive.server.WebTestClient
